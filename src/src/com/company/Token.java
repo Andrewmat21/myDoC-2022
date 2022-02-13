e45d8e056d6c65a9cff1f9284e06d83a88b0e1f9
Andrewmat21 <46943818+Andrewmat21@users.noreply.github.com>
2022-02-13 16:28:42 -0500

    added overrided Token constructor for different tokens

--- src/src/com/company/Token.java
+++ src/src/com/company/Token.java
@@ -5,10 +5,21 @@ public class Token {
     public String tokenType;
     public int lineNum;
     public int position;
+    public int value;
+    public String word;
 
-    public Token(String tokenType, int lineNum, int position){
+
+    public Token(String tokenType, int lineNum, int position, int value){
         this.tokenType = tokenType;
         this.lineNum = lineNum;
         this.position = position;
+        this.value = value;
+    }
+
+    public Token(String tokenType, int lineNum, int position, String word){
+        this.tokenType = tokenType;
+        this.lineNum = lineNum;
+        this.position = position;
+        this.word = word;
     }
 }
