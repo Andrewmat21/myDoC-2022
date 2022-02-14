9b0f924d77eea3a25b69699fecbbbc0e3e16c41a
Andrewmat21 <46943818+Andrewmat21@users.noreply.github.com>
2022-02-14 03:21:52 -0500

    added 3rd constructor for keyword tokens

--- src/src/com/company/Token.java
+++ src/src/com/company/Token.java
@@ -7,16 +7,24 @@ public class Token {
     public int position;
     public int value;
     public String word;
+    public char symbol;
 
 
-    public Token(String tokenType, int lineNum, int position, int value){
+    public Token(String tokenType, int lineNum, int position, int value) {
         this.tokenType = tokenType;
         this.lineNum = lineNum;
         this.position = position;
         this.value = value;
     }
 
-    public Token(String tokenType, int lineNum, int position, String word){
+    public Token(String tokenType, int lineNum, int position, String word) {
+        this.tokenType = tokenType;
+        this.lineNum = lineNum;
+        this.position = position;
+        this.word = word;
+    }
+
+    public Token(String tokenType, int lineNum, int position, char symbol) {
         this.tokenType = tokenType;
         this.lineNum = lineNum;
         this.position = position;
