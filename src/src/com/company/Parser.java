c47e08ed5636a51027b0d0f4b57e7109be503080
Andrewmat21 <46943818+Andrewmat21@users.noreply.github.com>
2022-03-08 20:03:16 -0500

    adding cases for stmt list

--- src/src/com/company/Parser.java
+++ src/src/com/company/Parser.java
@@ -12,110 +12,158 @@ import java.util.Scanner;
 public class Parser {
     int error = 0;
     int n = 0;
+    boolean flag;
     ArrayList<Token> stream = new ArrayList<Token>();
 
     public Parser(ArrayList<Token> tokenStream) {
         stream = tokenStream;
+        int errors = error;
     }
 
-    public boolean parseProgram() {
-
+    public void parseProgram() {
+        parseBlock();
+        match("EOP");
     }
 
-    public boolean parseBlock() {
-
+    public void parseBlock() {
+        match("L_BRACE");
+        parseStatementList();
+        match("R_BRACE");
     }
 
-    public boolean parseStatementList() {
-
+    public void parseStatementList() {
+        flag = false;
+        switch(getToken().tokenType){
+            case "PRINT":
+                flag = true;
+                break;
+            case "ID":
+                flag = true;
+                break;
+            case "VAR_TYPE":
+                flag = true;
+                break;
+            case "WHILE":
+                flag = true;
+                break;
+            case "IF":
+                flag = true;
+                break;
+        }
+        if (flag){
+            parseStatement();
+            parseStatementList();
+        }
+        else
+            n += 0;
     }
 
-    public boolean parsePrintStatement() {
-
-    }
-
-    public boolean parseAssignmentStatement() {
-
-    }
-
-    public boolean parseVarDecl() {
-
-    }
-
-    public boolean parseWhileStatement() {
-
-    }
-
-    public boolean parseIfStatement() {
-
-    }
-
-    public boolean parseExpr() {
-
-    }
-
-    public boolean parseIntExpr() {
-
-    }
-
-    public boolean parseStringExpr() {
-
-    }
-
-    public boolean parseBoolExpr() {
+    public void parseStatement(){
 
     }
 
-    public boolean parseId() {
+    public void parsePrintStatement() {
 
     }
 
-    public boolean parseCharList() {
+    public void parseAssignmentStatement() {
 
     }
 
-    public boolean parseType() {
+    public void parseVarDecl() {
 
     }
 
-    public boolean parseChar() {
+    public void parseWhileStatement() {
 
     }
 
-    public boolean parseDigit() {
+    public void parseIfStatement() {
 
     }
 
-    public boolean parseBoolOp() {
+    public void parseExpr() {
 
     }
 
-    public boolean parseBoolVal() {
+    public void parseIntExpr() {
 
     }
 
-    public boolean parseIntOp() {
+    public void parseStringExpr() {
+
+    }
+
+    public void parseBoolExpr() {
+
+    }
+
+    public void parseId() {
+
+    }
+
+    public void parseCharList() {
+
+    }
+
+    public void parseType() {
+
+    }
+
+    public void parseChar() {
+
+    }
+
+    public void parseDigit() {
+
+    }
+
+    public void parseBoolOp() {
+
+    }
+
+    public void parseBoolVal() {
+
+    }
+
+    public void parseIntOp() {
 
     }
 
     public boolean match(String expected) {
         Token currTok = getToken();
-        if (currTok.value.equals(expected)) {
+        if (currTok.tokenType == expected) {
+            // addNode
+            if (n < stream.size() - 1) {
+                n++;
+            }
             return true;
-            n++;
         }
-    }
 
+        else
+            error++;
+            throwError(currTok.tokenType, expected, currTok.lineNum, currTok.position);
+            return false;
+    }
+/*
     public boolean match(char expected) {
         Token currTok = getToken();
-        if (currTok.symbl.equals(expected)) {
+        if (currTok.symbl == expected) {
             // addNode
         }
 
-    }
+        else {
+            error++;
+            throwError();
+        }
+    }*/
 
     public Token getToken() {
        return stream.get(n);
     }
 
+    public static void throwError(String tokenType, String expected, int line, int position){
+        System.out.println("ERROR Parser - Expected [ " + expected + " ] but found [ " + tokenType + " ] at (" + line + ":" + (position+1) + ")");
+
+    }
 }
