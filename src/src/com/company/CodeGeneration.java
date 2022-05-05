5e53ef5f949fd9a0401839bd6eb7080f80bf930c
Andrewmat21 <46943818+Andrewmat21@users.noreply.github.com>
2022-05-05 18:59:14 -0400

    code gen

---
+++ src/src/com/company/CodeGeneration.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class CodeGeneration {
+
+    int position = 0;
+    String generatedCode[] = new String[255];
+    
+
+    public void generate(CSTNode n, int progNum){
+        switch (n.type){
+            case "VarDecl":
+            case "AssignmentStatement":
+            case "PrintStatement":
+            case "Block":
+            case "Addition":
+            case "Equality":
+            case "Inequality":
+            case "":
+        }
+    }
+}
